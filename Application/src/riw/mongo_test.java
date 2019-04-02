0a4c8d971bed9833bf2410d932ebb1e2b9689765
alex_ <alex_@ALEX-ROG>
2019-04-02 15:32:36 +0300

    Added mongo_test to test the connection to mongoDB

---
+++ Application/src/riw/mongo_test.java
@@ -0,0 +1,27 @@
+package riw;
+
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class mongo_test {
+
+	public static void main(String[] args) {
+		MongoClient mongoClient = new MongoClient("localhost", 27017) ;
+		MongoDatabase database = mongoClient.getDatabase("mydb");
+		MongoCollection<Document> collection = database.getCollection("test");
+		Document doc = new Document("name", "MongoDB")
+				.append("type", "database")
+				.append("count", 1)
+				.append("info", new Document("x", 203).append("y", 102));
+		
+		collection.insertOne(doc);
+		
+		//Document myDoc = collection.find(eq("i", 71)).first();
+		//System.out.println(myDoc.toJson());
+	}
+
+}
