df8d151670fc2ebb5f0a1c37666aabfb9f4a66fd
alexgrigoras <alex_grigoras_10@yahoo.com>
2019-04-06 14:35:06 +0300

    Refactored comments and added parameter for extension type

--- Application/src/riw/FileExplorer.java
+++ Application/src/riw/FileExplorer.java
@@ -14,19 +14,55 @@ import java.util.Queue;
  * 
  */
 public class FileExplorer {
-	private Queue<String> filesQueue;						// queue for processing links
-	
-	// Constructor
-	public FileExplorer() {
+	/**
+	 * Variables
+	 */
+	private Queue<String> filesQueue;				// queue for processing links
+	private String extension;						// extension for searched files
+
+	/**
+	 * Class constructor
+	 * @param _extension
+	 */
+	public FileExplorer(String _extension) {
 		filesQueue = new LinkedList<String>();
+		extension = _extension;
 	}
 	
-	// Returns the files queue
+	/**
+	 * Returns the files queue
+	 * @return the list of files
+	 */
 	public Queue<String> getFiles() { 
 		return filesQueue;
 	}
+
+	/**
+	 * Get a queue with file names and path
+	 * @param nr_files
+	 * @return a queue with files
+	 */
+	public Queue<String> getFiles(int nr_files) {
+		Queue<String> oldfilesQueue = filesQueue;
+		Queue<String> newfilesQueue = new LinkedList<String>();
+		
+		if(nr_files <= oldfilesQueue.size()) {
+			for(int i=0; i< nr_files; i++) {
+				newfilesQueue.add(oldfilesQueue.poll());
+			}
+			
+			return newfilesQueue;
+		}
+		
+		return oldfilesQueue;
+	}
 	
-	// Searches for the files in a specified folder recursively
+	/**
+	 * Searches for the files in a specified folder recursively
+	 * @param _folder_path
+	 * @param max_level
+	 * @param current_level
+	 */
 	public void searchFiles(String _folder_path, int max_level, int current_level) {
 		if(current_level > max_level && max_level != 0) {
 			return;
@@ -37,7 +73,7 @@ public class FileExplorer {
         File[] fileList = fileName.listFiles();
         for (File file: fileList) {
             if(file.isFile()) {
-            	if(getFileExtension(file).equals("txt")) {
+            	if(getFileExtension(file).equals(extension)) {
                 	filesQueue.add(file.toString());
             	}
             }
@@ -47,13 +83,17 @@ public class FileExplorer {
             
         }		
 	}
-	
-	// Returns the file extension from the file name
+
+	/**
+	 * Returns the file extension from the file name
+	 * @param file
+	 * @return
+	 */
 	private String getFileExtension(File file) {
 	    String name = file.getName();
 	    int lastIndexOf = name.lastIndexOf(".");
 	    if (lastIndexOf == -1) {
-	        return ""; 											// empty extension
+	        return ""; 								// empty extension
 	    }
 	    return name.substring(lastIndexOf + 1).toString();
 	}
