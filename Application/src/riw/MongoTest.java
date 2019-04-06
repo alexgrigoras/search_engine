eb589bae733a9d3b3c8c2c4b5f66f86283ae3a76
alexgrigoras <alex_grigoras_10@yahoo.com>
2019-04-06 14:34:29 +0300

    Refactored name

--- Application/src/riw/MongoTest.java
+++ Application/src/riw/MongoTest.java
@@ -7,7 +7,7 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-public class mongo_test {
+public class MongoTest {
 
 	public static void main(String[] args) {
 		MongoClient mongoClient = new MongoClient("localhost", 27017) ;
