a584aef1d2f2351d23ad5f1f5d3b34d7b540157f
alexgrigoras <alex_grigoras_10@yahoo.com>
2019-04-08 23:19:19 +0300

    Added a few testing routines in main function

--- Application/src/riw/DatabaseModule.java
+++ Application/src/riw/DatabaseModule.java
@@ -17,6 +17,7 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
 public class DatabaseModule {
@@ -61,7 +62,9 @@ public class DatabaseModule {
 	}
 
 	public static void main(String[] args) {
-		DatabaseModule dm = new DatabaseModule("tf_values");
+		DatabaseModule dm = new DatabaseModule("inverse_index_values");
+		
+		// TESTING module
 		
 		/*
 		List<Document> documents = new ArrayList<Document>();
@@ -75,12 +78,60 @@ public class DatabaseModule {
 		dm.insertDoc(docum);
 		*/		
 
+		/*
 		BasicDBObject criteria = new BasicDBObject();
 		criteria.append("doc", "E:\\Facultate\\Anul IV - Facultate\\Semestrul I\\ALPD - Algoritmi paraleli si distribuiti\\Tema de casa\\test-files\\test-files\\2.txt");
 		criteria.append("k", "cancel");
 				
 		Document myDoc = dm.collection.find(criteria).first();
 		System.out.println(myDoc.get("tf"));
+		*/
+		
+		/*
+		Document myDoc = dm.collection.find(eq("term", "mistreat")).first();
+		try {
+			List<Document> foundDoc = (List<Document>)myDoc.get("docs");
+			
+			Document docum = new Document("d", "abc")
+         			.append("c", 44);
+			
+			foundDoc.add(docum);
+			
+			System.out.println(foundDoc.toString());
+			
+			Document newDocument = new Document("term", "mistreat")
+					.append("docs", foundDoc);
+			
+			//dm.collection.updateOne(eq("term", "mistreat"), new Document("$set", newDocument));
+		}
+		catch(NullPointerException ex) {
+			System.out.println("not found");
+		}		
+		*/
+		
+		/*
+		Document myDoc = dm.collection.find(eq("term", "illumin")).first();
+		
+		List<Document> links = (List<Document>) myDoc.get("docs");
+		
+		for (Document link : links) {
+			System.out.println(link.get("d"));
+			System.out.println(link.get("c"));
+		}
+		
+		System.out.println();
+		*/
+		
+		/*
+		MongoCursor<Document> cursor = dm.collection.find(eq("term", "mistreat")).iterator();
+		try {
+		    while (cursor.hasNext()) {
+		        System.out.println(cursor.next().toJson());
+		    }
+		} finally {
+		    cursor.close();
+		}
+		*/
 
 		//Document myDoc = collection.find().first();
 		//System.out.println(myDoc.toJson());
