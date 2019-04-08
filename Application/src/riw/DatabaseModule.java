a203da6ee5abb0b2c05e106517f6909cd567b068
alexgrigoras <alex_grigoras_10@yahoo.com>
2019-04-08 23:46:26 +0300

    Updated main function to test the inverse index db

--- Application/src/riw/DatabaseModule.java
+++ Application/src/riw/DatabaseModule.java
@@ -62,79 +62,16 @@ public class DatabaseModule {
 	}
 
 	public static void main(String[] args) {
+		// Testing the module
 		DatabaseModule dm = new DatabaseModule("inverse_index_values");
-		
-		// TESTING module
-		
-		/*
-		List<Document> documents = new ArrayList<Document>();
-		for (int i = 0; i < 100; i++) {
-		    documents.add(new Document("i", i));
-		}
-		
-		Document docum = new Document("doc", "abc")
-				.append("terms", documents);
-		
-		dm.insertDoc(docum);
-		*/		
 
-		/*
-		BasicDBObject criteria = new BasicDBObject();
-		criteria.append("doc", "E:\\Facultate\\Anul IV - Facultate\\Semestrul I\\ALPD - Algoritmi paraleli si distribuiti\\Tema de casa\\test-files\\test-files\\2.txt");
-		criteria.append("k", "cancel");
-				
-		Document myDoc = dm.collection.find(criteria).first();
-		System.out.println(myDoc.get("tf"));
-		*/
-		
-		/*
-		Document myDoc = dm.collection.find(eq("term", "mistreat")).first();
-		try {
-			List<Document> foundDoc = (List<Document>)myDoc.get("docs");
-			
-			Document docum = new Document("d", "abc")
-         			.append("c", 44);
-			
-			foundDoc.add(docum);
-			
-			System.out.println(foundDoc.toString());
-			
-			Document newDocument = new Document("term", "mistreat")
-					.append("docs", foundDoc);
-			
-			//dm.collection.updateOne(eq("term", "mistreat"), new Document("$set", newDocument));
-		}
-		catch(NullPointerException ex) {
-			System.out.println("not found");
-		}		
-		*/
-		
-		/*
-		Document myDoc = dm.collection.find(eq("term", "illumin")).first();
-		
+		Document myDoc = dm.collection.find(eq("term", "appl")).first();
+
 		List<Document> links = (List<Document>) myDoc.get("docs");
 		
 		for (Document link : links) {
 			System.out.println(link.get("d"));
 			System.out.println(link.get("c"));
 		}
-		
-		System.out.println();
-		*/
-		
-		/*
-		MongoCursor<Document> cursor = dm.collection.find(eq("term", "mistreat")).iterator();
-		try {
-		    while (cursor.hasNext()) {
-		        System.out.println(cursor.next().toJson());
-		    }
-		} finally {
-		    cursor.close();
-		}
-		*/
-
-		//Document myDoc = collection.find().first();
-		//System.out.println(myDoc.toJson());
 	}
-
 }
